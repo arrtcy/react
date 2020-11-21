7e8afbae8654985da6b3213ae835d53230a37200
arrtcy <2510356829@qq.com>
2020-11-21 14:47:19 +0800

    路由改变

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import "./App.css";
-import NavBottom from "./components/NavBottom";
-import SearchTop from "./components/SearchTop";
+
 import Main from "./Main";
 import { Switch, Route, withRouter } from "react-router-dom";
+import Login from "./pages/login/login";
+import Home from "./pages/home/index";
+import Cart from "./pages/cart/index";
+import Account from "./pages/account/index";
+import User from "./pages/user/index";
 
 function App() {
   return (
     <>
-      <SearchTop />
-      <Main></Main>
-      <NavBottom />
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+       
+        <Main>
+        <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+        </Main>
+       
+
+      </Switch>
     </>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 
-function Index() {
+function Index(props) {
+  console.log(props);
   return (
     <div>
       <h2>首页页</h2>
+      <button onClick={()=>props.history.push('/login')}> 登录</button>
     </div>
   )
 }
 
-export default Index
+export default withRouter(Index)
